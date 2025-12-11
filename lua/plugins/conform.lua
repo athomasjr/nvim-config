6ce2b1de63599364c7a855f4f3f6331a5208a070
Antonio Thomas <antoniothomasjr@gmail.com>
2025-12-11 05:36:48 -0600

    feat(format): use golangci-lint for go formatting

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -1,6 +1,18 @@
 return {
   {
     "stevearc/conform.nvim",
-    opts = {},
+    opts = {
+      formatters_by_ft = {
+        go = { "golangci_lint" },
+      },
+      formatters = {
+        golangci_lint = {
+          command = "golangci-lint",
+          args = { "fmt", "--stdin" },
+          stdin = true,
+          cwd = require("conform.util").root_file({ ".golangci.yml", ".golangci.yaml", "go.mod" }),
+        },
+      },
+    },
   },
 }
