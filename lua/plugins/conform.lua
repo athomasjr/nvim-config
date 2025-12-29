5b9a15b255be542f648e0b78100ce0317e38c68c
Antonio Thomas <antoniothomasjr@gmail.com>
2025-12-28 19:27:13 -0600

    feat: add sql formatter config and new plugins

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -1,18 +1,30 @@
 return {
   {
     "stevearc/conform.nvim",
-    opts = {
-      formatters_by_ft = {
-        go = { "golangci_lint" },
-      },
-      formatters = {
-        golangci_lint = {
-          command = "golangci-lint",
-          args = { "fmt", "--stdin" },
-          stdin = true,
-          cwd = require("conform.util").root_file({ ".golangci.yml", ".golangci.yaml", "go.mod" }),
+    opts = function(_, opts)
+      -- Extend existing formatters_by_ft instead of replacing
+      opts.formatters_by_ft = opts.formatters_by_ft or {}
+      opts.formatters_by_ft.go = { "golangci_lint" }
+      opts.formatters_by_ft.sql = { "sql_formatter" }
+
+      -- Add custom formatter configurations
+      opts.formatters = opts.formatters or {}
+      opts.formatters.golangci_lint = {
+        command = "golangci-lint",
+        args = { "fmt", "--stdin" },
+        stdin = true,
+        cwd = require("conform.util").root_file({ ".golangci.yml", ".golangci.yaml", "go.mod" }),
+      }
+      opts.formatters.sql_formatter = {
+        prepend_args = {
+          "--language",
+          "postgresql",
+          "--config",
+          '{"keywordCase": "upper"}',
         },
-      },
-    },
+      }
+
+      return opts
+    end,
   },
 }
